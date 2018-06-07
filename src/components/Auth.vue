40d29262b820d9611b1b7f8a8c71b7960a0d7c7a
Wangding <506759656@qq.com>
2018-06-07 20:02:46 +0800

    .

---
+++ src/components/Auth.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  mounted(){
+
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/auth/Login'
+import Auth from '@/components/Auth'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'home',
       component: HelloWorld
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: Auth,
+      children: [
+        {
+          path: 'login',
+          name: 'Login',
+          component: Login
+        },
+      ]
     }
   ]
 })
